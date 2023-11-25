08ec2a92d9b1c54f8a95e7ce26fe7e4319b5bf0c
piyushmittal50 <mittalpiyush87483@gmail.com>
2023-11-25 17:45:42 +0530

    Function_in_js

--- Function_in_js/function_01.js
+++ Function_in_js/function_01.js
@@ -1,12 +1,40 @@
 // JavaScript functions are defined with the function keyword
-//                                           ---------
+//  01                                         ---------
 function greeting() {
     console.log("hello piyush")
 }
-
 greeting()
 
+// 02
 function addTwo(num1, num2) {
     console.log( num1 + num2);
 }
 addTwo(5, 10);
+
+// 03
+function squareOf(n) {
+    return n * n;
+}
+const ans = squareOf(2);
+console.log("ans is : ", ans)
+
+// 04
+function loggedIn(username) {
+    if (username === undefined) {
+        console.log("Please Enter a username !")
+        return;
+    }
+
+    return `${username} just logged in`;
+}
+console.log(loggedIn("piyush"));
+
+console.log(loggedIn()) // undefined just logged in
+
+
+// 05 => by giving default values
+function loggedout(username = "sam") {
+    return `${username} is just logout`;
+}
+console.log(loggedout("piyush"))
+console.log(loggedout())

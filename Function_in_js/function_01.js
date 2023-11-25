fdb7b54d49d0ab0c2da690eccebcdff3c2a70a1b
piyushmittal50 <mittalpiyush87483@gmail.com>
2023-11-25 17:18:00 +0530

    Functions_in_js

---
+++ Function_in_js/function_01.js
@@ -0,0 +1,7 @@
+// JavaScript functions are defined with the function keyword
+//                                           ---------
+function greeting() {
+    console.log("hello piyush")
+}
+
+greeting()
